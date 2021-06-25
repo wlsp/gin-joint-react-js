65fe8d5ef85318cd94d237c79566e4c55a8e41af
wlsp <wlsp@outlook.de>
2021-06-25 09:56:53 +0200

    20%

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
         <Route path="/shop" component={Shop} />
         <Route path="/contact" component={Contact} />
         <Route path="/" exact component={Home} />
-        
       </Switch>
       <Footer />
     </Router>
